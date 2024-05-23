f1347652ea1ac1f12b993e60df979d9400d5f65e
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-05-23 07:35:02 +0530

    docker kubernetes

--- 01-ibm-backend-express/src/config/db.connection.js
+++ 01-ibm-backend-express/src/config/db.connection.js
@@ -1,8 +1,14 @@
 import mongoose from 'mongoose';
 
 const connectionString = 'mongodb://localhost:27017';
+// const connectionString = 'mongodb://127.0.0.1:27017';
+// const connectionString = 'mongodb://192.168.137.1:27017';
 const databaseName = 'ibm';
 
 mongoose.connect(`${connectionString}/${databaseName}`)
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Error connecting to MongoDB:', err));
+
+
+
+
